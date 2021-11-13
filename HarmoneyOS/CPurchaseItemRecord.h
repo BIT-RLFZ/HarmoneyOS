c7dbf152716d787669f6a51d74dd3b76e83a53bc
Streamyue <630940124@qq.com>
2021-11-13 20:33:24 +0800

    两个构造函数以及重载==运算符的说明

--- HarmoneyOS/CPurchaseItemRecord.h
+++ HarmoneyOS/CPurchaseItemRecord.h
@@ -2,10 +2,27 @@
 #include "CItemInfo.h"
 class CPurchaseItemRecord
 {
+	/*
+		购物商品记录的判重
+		重载的 == 运算符
+		只要商品一样就认为一样
+		所属：冯宇鹏
+	*/
+	friend bool operator==(const CPurchaseItemRecord &RecordA, const CPurchaseItemRecord& RecordB);
 public:
 	CItemInfo Item;
 	double Weight; //购买重量
 	int Count; //购买个数
 	int Timestamp;
 	int OrderId; //物品所属订单号
+	/*
+		无参构造函数
+		仅初始化基本类型
+	*/
+	CPurchaseItemRecord();
+	/*
+		构造函数
+		为各个成员赋初值
+	*/
+	CPurchaseItemRecord(const CItemInfo& Item, double Weight, int Count, int TimeStamp, int OrderId);
 };
