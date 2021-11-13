5f4ed6ddfb812f48395824838110e66b004bee09
bobh <bobh@zendee.cn>
2021-11-13 19:19:43 +0800

    完成添加库存自动合并的单元测试

--- HarmoneyOSTest/DatabaseTest.cpp
+++ HarmoneyOSTest/DatabaseTest.cpp
@@ -18,14 +18,18 @@ namespace HarmoneyOSTest
 	{
 	public:
 		Database database;
-
+		const int ItemsCount = 1000;
 		TEST_CLASS_INITIALIZE(ClassInitialize)
 		{
 			Logger::WriteMessage("[Database_Test] Start to Unittest Database Module.\n");
-			//删除以前的测试数据库文件，保证数据库是全新的以保证测试的正确性
+			// 删除以前的测试数据库文件，保证数据库是全新的以保证测试的正确性
 			remove("test.osdb");
-			//随机化种子，供后续随机加入商品使用
+		}
+		HarmoneyOS_Database_Test() {
+			// 随机化种子，供后续随机加入商品使用
 			srand((unsigned)time(0));
+			// 随机生成一堆商品
+			GenerateItems(ItemsCount);
 		}
 		TEST_CLASS_CLEANUP(ClassCleanup)
 		{
@@ -69,10 +73,66 @@ namespace HarmoneyOSTest
 			RdNameMp[ret] = true;
 			return ret;
 		}
+		std::vector<CItemInfo> items;
+		void GenerateItems(int count) {
+			for (int i = 0; i < count; i++) {
+				CItemInfo cur;
+				cur.ItemName = GetUniqueRandomName(15);
+				cur.Cost = rand() % 100 + 10;
+				cur.Price = cur.Cost + rand() % 20 + 1;
+				cur.ItemId = 1145140000 + i;
+				cur.ItemType = rand() % 2;
+				items.push_back(cur);
+			}
+		}
+
+		/*
+			随机生成一个商品库存信息
+		*/
+		CItemStorageInfo GenerateRandomItemStorageInfo() {
+			auto rd = rand();
+			CItemStorageInfo ret;
+			
+		}
+		/*
+			测试添加库存的功能，还要测试是否按要求合并正确
+		*/
 		TEST_METHOD(AddItemStorageInfoTest)
 		{
 			try {
-				
+				for (int i = 0; i < items.size(); i++) {
+					auto item = items[i];
+					CItemStorageInfo ret;
+					if (item.ItemType == 0) {
+						// 按个数卖
+						ret.CountRest = rand() % 100 + 10;
+						ret.IsDelete = false;
+						ret.Item = item;
+						ret.Timestamp = time(0);
+						ret.WeightRest = 0;
+					}
+					else {
+						// 按斤
+						ret.CountRest = 0;
+						ret.IsDelete = false;
+						ret.Item = item;
+						ret.Timestamp = time(0);
+						ret.WeightRest = (rand() + 200) / 100.0f + 10.0f;
+					}
+					auto status = database.AddItemStorageInfo(ret);
+					if (!status) Assert::Fail();
+					double newCost = ret.Item.Cost + 30;
+					double newPrice = ret.Item.Price + 40;
+					ret.Item.Cost = newCost;
+					ret.Item.Price = newPrice;
+					status = database.AddItemStorageInfo(ret);
+					if (!status) Assert::Fail();
+					auto dataRes = database.QueryItemStorageInfo(ret.Item.ItemId);
+					if(dataRes.Item.Cost != newCost) Assert::Fail();
+					if (dataRes.Item.Price != newPrice) Assert::Fail();
+					if(dataRes.Item.ItemType == 0 && dataRes.CountRest != ret.CountRest * 2) Assert::Fail();
+					if (dataRes.Item.ItemType == 1 && dataRes.WeightRest != ret.WeightRest * 2) Assert::Fail();
+				}
 			}
 			catch (NoImplException ex) {
 				Logger::WriteMessage((ex.GetFunctionName() + "() throw NoImplException! (ignore)\n").c_str());
