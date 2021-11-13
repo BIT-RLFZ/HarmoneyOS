42c75e1275c9615885ccf72cb380709032597b18
Streamyue <630940124@qq.com>
2021-11-13 10:39:44 +0800

    CItemInfo重载==运算符

--- HarmoneyOS/CItemInfo.h
+++ HarmoneyOS/CItemInfo.h
@@ -3,6 +3,11 @@
 
 class CItemInfo
 {
+	/*
+		友元函数 重载 == 运算符
+		具体实现应该判断 ItemId 就够了
+	*/
+	friend bool operator==(const CItemInfo& item1, const CItemInfo& item2);
 public:
 	std::string	ItemName;
 	double Cost; //进价
