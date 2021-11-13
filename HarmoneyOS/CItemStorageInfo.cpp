c2b3db885b5cac859c68b0ff2925a3f28a74dcf5
Streamyue <630940124@qq.com>
2021-11-13 21:15:48 +0800

    添加两个构造函数

--- HarmoneyOS/CItemStorageInfo.cpp
+++ HarmoneyOS/CItemStorageInfo.cpp
@@ -1,1 +1,13 @@
 #include "CItemStorageInfo.h"
+
+CItemStorageInfo::CItemStorageInfo()
+{
+	WeightRest = 0.0;
+	CountRest = 0;
+	Timestamp = 0;
+	IsDelete = false;
+}
+
+CItemStorageInfo::CItemStorageInfo(const CItemInfo& Item, double WeightRest, int CountRest, int Timestamp, bool IsDelete):Item(Item), WeightRest(WeightRest), CountRest(CountRest), Timestamp(Timestamp), IsDelete(IsDelete)
+{
+}
--- HarmoneyOS/CItemStorageInfo.h
+++ HarmoneyOS/CItemStorageInfo.h
@@ -9,5 +9,15 @@ public:
 	int CountRest;
 	int Timestamp; //录入时间
 	bool IsDelete;
+	/*
+		无参构造
+		将基本数据类型赋为默认值
+	*/
+	CItemStorageInfo();
+	/*
+		有参构造
+		将所有成员赋初值
+	*/
+	CItemStorageInfo(const CItemInfo& Item, double WeightRest, int CountRest, int Timestamp, bool IsDelete);
 };
 
