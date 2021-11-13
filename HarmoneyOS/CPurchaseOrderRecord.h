79e50efa88205b645dd52a6b3da0d02daebd6c79
Streamyue <630940124@qq.com>
2021-11-13 20:18:32 +0800

    添加了两个构造函数

--- HarmoneyOS/CPurchaseOrderRecord.h
+++ HarmoneyOS/CPurchaseOrderRecord.h
@@ -2,7 +2,16 @@
 class CPurchaseOrderRecord
 {
 public:
-	int OrderId;
-	int Timestamp;
+	int OrderId;	//订单编号
+	int Timestamp;	//时间戳
+	/*
+		CPurchaseOrderRecord的无参构造
+	*/
+	CPurchaseOrderRecord();	
+	/*
+		有参构造
+		方便为两个成员赋初值
+	*/
+	CPurchaseOrderRecord(int OrderId, int Timestamp);
 };
 
