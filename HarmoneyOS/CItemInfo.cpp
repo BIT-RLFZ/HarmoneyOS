162babde7d4127483de6983bc30133bc0d416c15
Streamyue <630940124@qq.com>
2021-11-13 13:11:59 +0800

    CItemInfo重载==运算符，仅仅抛出未实现异常

--- HarmoneyOS/CItemInfo.cpp
+++ HarmoneyOS/CItemInfo.cpp
@@ -1,1 +1,7 @@
 #include "CItemInfo.h"
+#include "NoImplException.h"
+bool operator==(const CItemInfo& item1, const CItemInfo& item2)
+{
+    throw NoImplException(__FUNCTION__);
+    return false;
+}
