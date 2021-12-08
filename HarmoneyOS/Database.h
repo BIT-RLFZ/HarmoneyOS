f8a29608de58a8716315e737be4ce33123f4a063
Sjeary <1286860778@qq.com>
2021-12-08 10:25:31 +0800

    修改了注释变成乱码的问题

--- HarmoneyOS/Database.h
+++ HarmoneyOS/Database.h
@@ -12,7 +12,7 @@
 #include <set>
 
 /*
-	���ݿ��ļ��ṹ: designed by RBH
+	数据库文件结构: designed by RBH
 
 	=== Header ===
 
@@ -28,9 +28,9 @@
 */
 struct DatabaseHeader {
 	char magicChar[11]; //HarmoneyDB
-	int timestamp; //���ݿ���������ʱ��
-	int StringPoolOffset; //�ַ����ص��ļ�ƫ��
-	int StringPoolLength; //�ַ����صĳ���
+	int timestamp; //数据库最后更新时间
+	int StringPoolOffset; //字符串池的文件偏移
+	int StringPoolLength; //字符串池的长度
 	int AItemInfoPoolOffset;
 	int AItemInfoCount;
 	int AItemStorageInfoPoolOffset;
@@ -45,7 +45,7 @@ struct AbstractString {
 	int length; // string length
 };
 struct AbstractItemInfo {
-	int ItemDatabaseID; // �����ݿ��洢ʱ������UID
+	int ItemDatabaseID; // 在数据库存储时分配的UID
 	AbstractString ItemName; // Get From StringPool
 	AbstractString ItemId; // Get From StringPool
 	double Cost;
@@ -53,15 +53,15 @@ struct AbstractItemInfo {
 	int ItemType;
 };
 struct AbstractItemStorageInfo {
-	int Item; // ��ItemInfo�����ҵ�ItemDatabaseIDΪItem����Ŀ��Ϊ����
+	int Item; // 查ItemInfo表，找到ItemDatabaseID为Item的项目即为此项
 	double WeightRest;
 	int CountRest;
-	int Timestamp; //¼��ʱ��
+	int Timestamp; //录入时间
 	bool IsDelete;
 };
 
 struct AbstractPurchaseItemRecord {
-	int Item; // ��ItemInfo�����ҵ�ItemDatabaseIDΪItem����Ŀ��Ϊ����
+	int Item; // 查ItemInfo表，找到ItemDatabaseID为Item的项目即为此项
 	double Weight;
 	int Count;
 	int Timestamp;
@@ -78,7 +78,7 @@ public:
 	}
 
 	~Database() {
-		try{
+		try {
 			UpdateDatabaseFile();
 		}
 		catch (NoImplException ex) {};
@@ -87,68 +87,68 @@ public:
 		if (gStringPool) delete[] gStringPool;
 	}
 	/*
-		��ʼ�����ݿ⣬�������ݿ��ļ����ݲ�����
-		�ļ�������ʱ���Զ���ʼ��һ���µ����ݿ��ļ�
+		初始化数据库，读入数据库文件数据并解析
+		文件不存在时会自动初始化一个新的数据库文件
 	*/
-	bool InitDatabase(std::string dbFileName="Harmoney.osdb");
+	bool InitDatabase(std::string dbFileName = "Harmoney.osdb");
 	/*
-		��ѯָ��ItemId��Ʒ����Ʒ��Ϣ��������CItemStorageInfo
-		�����쳣��HarmoneyException
-			 �ڣ��޷��ҵ�ָ����ƷIDʱ
+		查询指定ItemId商品的商品信息，返回类CItemStorageInfo
+		可能异常：HarmoneyException
+			 在：无法找到指定商品ID时
 	*/
 	CItemStorageInfo QueryItemStorageInfo(std::string ItemId);
 	/*
-		�޸�ָ��ItemId��Ʒ����Ʒ��Ϣ������bool
-		�����쳣��HarmoneyException
-			 �ڣ��޷��ҵ�ָ����ƷIDʱ
+		修改指定ItemId商品的商品信息，返回bool
+		可能异常：HarmoneyException
+			 在：无法找到指定商品ID时
 	*/
 	bool ModifyItemStorageInfo(CItemStorageInfo& StorageInfo);
 	/*
-		��ָ����Ʒ�������ݿ⣬�Ѿ����ڵ���Ʒ�ᱻ�ϲ����ϲ������ǿ���ֱ�����ӣ����µĶ��󸲸ǽ����ۼ�����
+		将指定商品加入数据库，已经存在的商品会被合并，合并方法是库存直接相加，而新的对象覆盖进价售价数据
 	*/
 	bool AddItemStorageInfo(CItemStorageInfo& StorageInfo);
 	/*
-		��ѯ���ݿ������е���Ʒ������Ϣ��������Ʒ��Ϣ���б�
-		ע�⣡�������Ѿ��¼�(ɾ��)����Ʒ��Ȼ�᷵�أ�ע���ж� IsDelete
+		查询数据库中所有的商品库存信息，返回商品信息的列表
+		注意！！！，已经下架(删除)的物品依然会返回，注意判断 IsDelete
 	*/
 	std::vector<CItemStorageInfo>& GetAllItemStorageInfo();
 	/*
-		��ָ����Ʒ��Delete״̬���ó�true����ʾ��Ʒ�Ѿ��¼�
-		�����쳣��HarmoneyException
-			 �ڣ��޷��ҵ�ָ����ƷIDʱ
+		将指定商品的Delete状态设置成true，表示商品已经下架
+		可能异常：HarmoneyException
+			 在：无法找到指定商品ID时
 	*/
 	bool DeleteItemStorageInfo(std::string ItemId);
 
 	/*
-		����һ����Ʒ�Ĺ�����¼��ע��ͬһ�������п��ܻ����ɶ�����Ʒ������¼����Ϊ�����ж�����Ʒ������
+		添加一个商品的购买记录，注意同一个订单中可能会生成多个商品购买记录，因为可能有多个商品被购买
 	*/
 	bool AddPurchaseItemRecord(CPurchaseItemRecord& PurchaseItemRecord);
 	/*
-		��ȡ���ݿ���������Ʒ������¼
+		获取数据库中所有商品购买记录
 	*/
 	std::vector<CPurchaseItemRecord>& GetAllPurchaseItemRecord();
 
 	/*
-		���ݶ����ı��Ų�ѯָ������������ʱ��
+		根据订单的编号查询指定订单的消费时间
 	*/
 	CPurchaseOrderRecord QueryPurchaseOrderRecord(int OrderId);
 	/*
-		�����ݿ�����һ��������¼
+		向数据库加入一条订单记录
 	*/
 	bool AddPurchaseOrderRecord(CPurchaseOrderRecord& PurchaseRecord);
 	/*
-		��ȡ����һ�������ı��ţ������µĶ�������ʱ�������ţ�ֱ����������������ֵ+1����
-		û�ж���ʱ����0
+		获取最后一条订单的编号，用于新的订单生成时订单编号，直接用这个函数返回值+1即可
+		没有订单时返回0
 	*/
 	int GetLatestOrderId();
 	/*
-		��ѯ���ݿ������еĶ�����¼
+		查询数据库中所有的订单记录
 	*/
 	std::vector<CPurchaseOrderRecord>& GetAllPurchaseOrderRecord();
 	/*
-		�ѵ�ǰ�ڴ��е����ݿ��ļ��洢���ļ���
-		����ÿ�δ洢���ܻ�����һ����ʱ��(<100ms)��������ִ����һϵ���޸Ĳ������ٵ�����������
-		ע�⣺�ڳ����˳�ʱ���Զ�ִ���������䣬����Ϊ�˷�ֹ���⣬��ÿ�β������ݿ��󶼵��ô�����
+		把当前内存中的数据库文件存储到文件中
+		由于每次存储可能会消耗一定的时间(<100ms)，建议在执行完一系列修改操作后再调用这条语句
+		注意：在程序退出时会自动执行这条语句，但是为了防止意外，请每次操作数据库后都调用此语句
 	*/
 	bool UpdateDatabaseFile();
 private:
@@ -171,3 +171,4 @@ private:
 	std::string GetAbstractString(const AbstractString& as);
 	void LoadBinaryDBFile(std::string dbFileName);
 };
+
